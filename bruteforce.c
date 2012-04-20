bc3be6397a562af5dbed55c0520d4c82ed22a99b
Puneet <puneetv@seas.upenn.edu>
2012-04-20 18:50:52 -0400

    Trying to add the main function

--- bruteforce.c
+++ bruteforce.c
@@ -3,7 +3,15 @@
 #include <math.h>
 
 int main(){
-
-
+char message[256];
+long pubkey_e;// exponent of the public key
+long pubkey_mod;// mod componenet of public key
+printf("Enter the public key (exponent then modulo separated by space): \n");
+fgets(message, 255, stdin);
+char * _pubkey_e = strtok(message, " ");
+char * _pubkey_mod = strtok(NULL, " ");
+pubkey_e = atol(_pubkey_e);
+pubkey_mod = atol(_pubkey_mod);
+return 0;
 
 }
