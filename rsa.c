61f7f0fb4eae612771beff6c8581dd865b64c697
puneetv1987 <puneetv@seas.upenn.edu>
2012-04-20 12:31:49 -0300

    Update rsa.c

--- rsa.c
+++ rsa.c
@@ -2,6 +2,58 @@
 #include <stdlib.h>
 #include <time.h>
 
+int generatePrimeNumbers(int *m, int *n) {
+    int maximum = *m > *n ? *m : *n;
+    int *primeList = (int *)malloc(maximum * sizeof(int));
+    int index = 1; 
+    int i = 3;
+    primeList[0] = 2;
+    while(index < maximum) {
+        if (isPrime(i) == 1) {
+            primeList[index] = i;
+            index++;
+            
+        }
+        i += 2; 
+    }
+    
+    *m = primeList[*m - 1];
+    *n = primeList[*n - 1];     
+    free(primeList);
+
+int totient(int n) {
+    int *primeList  = (int *)malloc(n * sizeof(int));
+    primeList[0] = 2;
+    int j = 0;
+    int p;
+    int q;
+    int index = 1;
+    int i = 2;
+    for(i = 3; i < n; i +=2) {
+        if(isPrime(i) == 1) {
+            primeList[index] = i;
+            index++;
+        }
+    } 
+    for(i = 0; i < index; i++) {
+       // printf("prime = %d\n", primeList[i]);
+
+    }
+    //now we have the list in primeList, brute force to find the primeNUmbers
+    for(i = 0; i < index; i++) {
+        for (j = 0 ; j  <index; j++) {
+            if(primeList[i] * primeList[j] == n) {
+                p = primeList[i];
+                q = primeList[j];
+                free(primeList);
+                return (p - 1) * (q - 1);
+            }
+        }
+    }
+    free(primeList);
+    return -1;
+}
+
 // Generate a prime number
 long getPrime() {
   long value = 0;
@@ -329,3 +381,43 @@ int main() {
   printf("This is M %ld\n", getM(k, l));
 }
 
+/*
+void main() {
+srand(time(NULL));
+char *input = "Hello";
+int *encOutput = (int *)malloc(sizeof(int) * strlen(input) + 1);
+int c = 13 * 17;
+int i; 
+int a = 13;
+int b = 17;
+int e = coprime(12 * 16);
+int m = totient(13 * 17);
+int d = mod_inverse(e, 12 * 16); 
+printf("totient = %d", totient(13 * 17));
+printf("e = %d\n", coprime(12 * 16));
+
+
+
+printf("d = %d\n", mod_inverse(coprime(12 * 16), 12 * 16));
+int enc = encrypt(50, e,c);
+int dec = decrypt(enc, d, c);
+printf("enc = %d\n", enc);
+printf("dec = %d\n", dec);
+
+
+for(i = 0; i < strlen(input); i++) {
+    encOutput[i] = encrypt(input[i], e, c);
+     
+  
+}
+
+printf("Hello has been translated into %d %d %d %d %d\n", encOutput[0], encOutput[1], encOutput[2], encOutput[3], encOutput[4]);
+
+
+//    printf("coprime=%d\n",mod_inverse(3, 30));
+//printf("%d", modulo(2349723,423424,12345));
+//printf("mod inv = %d\n", extendedEuclid(442, 2278, &d, &x, &y));
+//printf("d = %d \t x = %d \t y = %d\n", d,x,y);
+//coprime(5);
+
+}*/
